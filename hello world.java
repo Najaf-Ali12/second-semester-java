cb8c77f8bffdba11cd83bb6173e0be01fafc7280
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-04-18 18:57:09 +0500

    Add files via upload

---
+++ hello world.java
@@ -0,0 +1,14 @@
+class helloworld{
+    public static void main(String args[]){
+        System.out.println("lets learn about pakistan");
+        System.out.print("what we are going to learn about pakistan.\n we will learn about history of pakistan");
+    }
+}
+//NAME OF file and the name of class should always be same 
+//in java every predifed function has first letter capital
+// public word means that it can be accessible from the outside of the class
+// void means that function will not return anything
+// we write println to go to next line, it can run with print() as well but the new thing will not be on new line
+// system is predefined clause and if there is dot after class it means we are going to write any 
+//static member of that class, here out is static reference variable
+
